35b6f2d2c7c6d00d2aff2dd827147acf2f0404cb
Ismael0921-web <2007ismaelg@gmail.com>
2026-01-07 12:41:16 +0100

    Update xifrat_hibrid.py

--- xifrat_hibrid.py
+++ xifrat_hibrid.py
@@ -1,22 +1,22 @@
 from cryptography.fernet import Fernet
-import os   # Usamos os en lugar de Path
+import os
 
 
 # Nombre del archivo donde se guarda la clave
 CLAU_FILE = "clau_fernet.key"
 
 
-def guardar_clau(clau: bytes):
-    # Abrimos el archivo en modo escritura binaria
+def guardar_clau(clau):
+    # Abrimos el archivo en modo escritura binaria (se utiliza para guaradar claves)
     with open(CLAU_FILE, "wb") as f:
         # Escribimos la clave en el archivo
         f.write(clau)
 
 
-def carregar_clau() -> bytes:
+def carregar_clau():
     # Comprobamos si el archivo de la clave existe
     if os.path.exists(CLAU_FILE):
-        # Si existe, abrimos el archivo y leemos la clave
+        # Si existe, abrimos el archivo y leemos la clave binaria
         with open(CLAU_FILE, "rb") as f:
             return f.read()
 
@@ -25,20 +25,18 @@ def carregar_clau() -> bytes:
 
     # Guardamos la nueva clave
     guardar_clau(clau)
-
-    # Devolvemos la clave
+    
     return clau
 
 
-def xifrar_text(text: str, clau: bytes) -> bytes:
-    # Creamos el objeto Fernet con la clave
-    f = Fernet(clau)
+def xifrar_text(text, clau):  
+    f = Fernet(clau) # Creamos el objeto Fernet con la clave
 
     # Convertimos el texto a bytes y lo ciframos
     return f.encrypt(text.encode())
 
 
-def desxifrar_text(token: bytes, clau: bytes) -> str:
+def desxifrar_text(token, clau):
     # Creamos el objeto Fernet con la clave
     f = Fernet(clau)
 
