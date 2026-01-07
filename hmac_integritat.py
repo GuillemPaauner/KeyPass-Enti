bd28ce7c66f151ec47d82c3eb5336add9130cc99
Ismael0921-web <2007ismaelg@gmail.com>
2026-01-07 12:10:33 +0100

    Update hmac_integritat.py

--- hmac_integritat.py
+++ hmac_integritat.py
@@ -1,29 +1,25 @@
-import hmac        # Librería para crear firmas seguras
-import hashlib     # Librería para usar algoritmos hash
+import hmac        # LibrerÃ­a para crear firmas seguras
+import hashlib     # LibrerÃ­a para usar algoritmos hash
 
-# Clave secreta que SOLO conoce el programa
-# Se usa para firmar los datos
-CLAU_SECRETA = b"clau-super-secreta-keypass-2024"   
+# Clave secreta que solo conoce el programa, la cual se usa para firmar los datos
+CLAU_SECRETA = b"Clau_Secreta_Keypass"   
 
 
 def generar_hmac(dades):
 
-    # Creamos la firma usando:
-    # - la clave secreta
-    # - los datos
-    # - el algoritmo SHA-256
+    # Creamos la firma usando la clave secreta, los datos y el algoritmo SHA-256
     firma = hmac.new(CLAU_SECRETA, dades, hashlib.sha256)
 
-    # Convertimos la firma a texto para poder guardarla
+    # Convertimos la firma a texto hexadecimal para poder guardarla
     return firma.hexdigest()
 
 
 def verificar_hmac(dades, hmac_guardat):
+
     # Volvemos a crear la firma con los datos actuales
     hmac_actual = generar_hmac(dades)
 
-    # Comparamos la firma nueva con la firma guardada
-    # Si son iguales ? los datos NO han cambiado
+    # Comparamos la firma nueva con la firma guardada, y si son iguales devuleve true sino es que ha sido cambiada y devuelve false
     if hmac_actual == hmac_guardat:
         return True
     else:
