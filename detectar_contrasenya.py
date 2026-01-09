d1110187233605c3021221573ebf63b5d511ff40
diidac <didac.gonzalez@estudiant.enti.cat>
2026-01-09 18:24:09 +0100

    Update comentaris i traducció

--- detectar_contrasenya.py
+++ detectar_contrasenya.py
@@ -5,12 +5,12 @@ def es_contrasenya_feble(contrasenya, llista_existents):
     if len(contrasenya) < 8:       # Mira la longitud de la contrasenya
         return True
     
-    if contrasenya in llista_existents:    #Si existe devuelve true
+    if contrasenya in llista_existents:    #Si existeix retorna, true
         return True
 
-    patrones = [r"[A-Z]",r"[a-z]",r"[0-9]",r"[!@#$%^&*()\-_=+\[\]{};:,.?/]"]  # Se utiliza rejex para ver patrones como abc, letras o caracteres, i la r" para que reconozca /n
+    patrones = [r"[A-Z]",r"[a-z]",r"[0-9]",r"[!@#$%^&*()\-_=+\[\]{};:,.?/]"]  # S'utiliza rejex per veure patrons com abc, lletres o caracters, i la r" per a que reconegui /n
     
-    # Mira los patrones que tiene que seguir para ver si es una contrasena fuerta, comprarando el aptron con la contra, en el caso que no sea devulve True, que la contrasena es debil
+    # Mira els patrons que ha que seguir per veure si es una contrasena forta, comprarant el patró amb la contrasenya, en el cas que no ho sigui retorna True, que la contrasenya es debil.
     for patro in patrones:
         if not re.search(patro, contrasenya):
             return True
