81982f7d31c1980407462341fcfabcbe556f3c9f
diidac <didac.gonzalez@estudiant.enti.cat>
2026-01-09 18:28:40 +0100

    Actualització comentaris i traducció

--- generar_contrasenya.py
+++ generar_contrasenya.py
@@ -2,25 +2,25 @@ import string
 import random
 
 def generar_contrasenya(longitud):  
-    if longitud < 8:             # Si la contrasenya tiene menos de 8 caracteres entonces da error
+    if longitud < 8:             # Si la contrasenya té menys de 8 caracters, dona error
         raise ValueError("La longitud minima es 8 carcters.")
 
-    lletres_majus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"        # Caracteres possibles para la contrasenya
+    lletres_majus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"        # Caracters possibles per a la contrasenya
     lletres_minus = "abcdefghijklmnopqrstuvwxyz"
     nums = "0123456789"
     simbols = "!@#$%^&*()-_=+[]{};:,.?/"
     
-    # Se elijen primero 1 de cada una de las 4 listas para que haya almenos 1 caracter diferente
+    # S'escolleixen primer 1 de cada una de las 4 llistes per a que hi hagi almenys 1 caracter diferent
     contrasenya = [random.choice(lletres_majus),random.choice(lletres_minus),random.choice(nums),random.choice(simbols)]
     
-    # Se suman todas los caracterses quedando ABCD....abc....012....
+    # Se sumen tots els caràcters quedant ABCD....abc....012....
     total = lletres_majus + lletres_minus + nums + simbols
     
-    # Se van eligiendo randoms de la lista total hasta ser igual a la longitud elegida
+    # Es van escollint valors aleatoris de la llista total fins a arribar a la longitud desitjada
     while len(contrasenya) < longitud:
         contrasenya.append(random.choice(total))
     
-    # Se mezcla todo para evitar que haya possibles patrones
+    # Es barregen els caràcters per a que no segueixin un patró concret
     random.shuffle(contrasenya)
     return "".join(contrasenya)
     print(contrasenya)
